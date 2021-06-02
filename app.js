ad66e4d46fada8d3b414fbe3181e67cec0c5b399
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-06-02 16:43:56 -0700

    added pronoun and span tags with id for name and pronoun

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-console.log("hello!")
-const myElement = document.getElementById('top-section');
-console.log(myElement);
+// HTML setup
+// Grab the DOM elements (the input box and the button) in
+// Add an event listener to the button
+// Get access to the user's input
+// Use the user's input to "do something"
+// Display the results to the user
+
+const submitBtn = document.getElementById('submit-btn');
+const inputField = document.getElementById('input-field');
+const pronounField = document.getElementById('pronoun-field')
+const middleSection = document.getElementById('middle-section');
+const colorField = document.getElementById('color-field');
+const topSection = document.getElementById('top-section');
+const bottomSection = document.getElementById('bottom-section');
+const btn = document.getElementById('btn')
+const nameOutput = document.getElementById('name-output')
+const pronoun = document.getElementById('pronoun')
+
+submitBtn.addEventListener('click', ()=>{
+    console.log(inputField.value);
+     console.log(middleSection.textContent);
+    console.log(colorField.value);
+    topSection.style.backgroundColor = colorField.value;
+    bottomSection.style.backgroundColor = colorField.value;
+    nameOutput.textContent = inputField.value.toUpperCase();
+    
+})
+btn.addEventListener('click', ()=>{
+    console.log(pronounField.value);
+    pronoun.textContent = pronounField.value.toUpperCase();
+})
